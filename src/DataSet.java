0f8db82a730b5de7acfc380518432eaf9cc7748a
EmmanuelArevalo09 <162363524+EmmanuelArevalo09@users.noreply.github.com>
2024-06-04 12:05:22 -0500

    Primer commit

---
+++ src/DataSet.java
@@ -0,0 +1,17 @@
+public class DataSet {
+    // Método para obtener los datos del conjunto de datos
+    public static double[][] getData() {
+        // Definimos los datos
+        double[][] data = {
+                {-3, 7.5},
+                {-2, 3},
+                {-1, .5},
+                {0, 1},
+                {1, 3},
+                {2, 6},
+                {3, 14},
+                // Puedes agregar más datos según sea necesario
+        };
+        return data;
+    }
+}
